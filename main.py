b5b29c8dc9179fab03ebdfcc728c762fdf8e75a7
danieljerabek <jerd05@vse.cz>
2025-12-03 21:08:08 +0100

    Úprava kódu podle hodnocení projektu.

--- main.py
+++ main.py
@@ -2,16 +2,23 @@ import random
 import string
 import time
 
+# oddělovač pro lepší čitelnost hry
+ODDELOVAC = "-" * 47
+
 def generovani_cisla():
     '''
     Funkce slouží k vygenerování čtyřmístného čísla
     '''
+    # string.digits == "0123456789"; string.digits[1:] == "123456789" → vyloučíme 0
     prvni_cislice = random.choice(string.digits[1:])
-    ostatni_cislice = set(string.digits)
+    # vytvořen set z číslic 0-9
+    ostatni_cislice = list(string.digits)
+    # z listu odstraněna první číslece - tedy 0
     ostatni_cislice.remove(prvni_cislice)
+    # složení finálního čísla, která nezačíná 0 a má 4 číslice
     cislice = [prvni_cislice] + random.sample(ostatni_cislice, 3)
-    n = int(''.join(cislice))
-    return str(n)
+    n = ''.join(cislice)
+    return n
 
 def je_tip_validni(hracovo_cislo):
     '''
@@ -19,22 +26,22 @@ def je_tip_validni(hracovo_cislo):
     '''
     if len(hracovo_cislo) != 4:
         return False
-    elif not hracovo_cislo.isdigit():
+    if not hracovo_cislo.isdigit():
         return False
-    elif len(set(hracovo_cislo)) != 4:
+    if len(set(hracovo_cislo)) != 4:
         return False
-    elif hracovo_cislo[0] == "0":
+    if hracovo_cislo[0] == "0":
         return False
     else:
         return True
+ 
     
 def logika_kontrolovani(generovane_cislo, hracovo_cislo):
     '''
     Vrátí počet bulls a cows.
     '''
-    hracovo_cislo = str(hracovo_cislo)
-    bull_cow = [0,0]
-    for i,j in  zip(generovane_cislo, hracovo_cislo):
+    bull_cow = [0, 0]
+    for i, j in zip(generovane_cislo, hracovo_cislo):
         if j in generovane_cislo:
             if j == i:
                 bull_cow[0] += 1
@@ -42,65 +49,68 @@ def logika_kontrolovani(generovane_cislo, hracovo_cislo):
                 bull_cow[1] += 1
     return bull_cow
 
+
 def pluralize(bull_cow, word):
     '''
-    Pokud je počet cows nebo bull větší než jedna přidá "s" jako množné číslo.
+    Pokud je počet cows nebo bull větší než 1 nebo rovna 0 přidá "s" jako množné číslo.
     '''
-    if bull_cow > 1:
+    if bull_cow == 0 or bull_cow > 1:
         words = word + "s"
         return words
     else:
         return word
+
     
 def uvod_hry():
     '''
     Funkce slouží ke generování úvodu hry a generování náhodného čísla
     '''
     generovane_cislo = generovani_cisla()
-    oddelovac = "-----------------------------------------------"
     print("Hi there!")
-    print(oddelovac)
+    print(ODDELOVAC)
     print("I've generated a random 4 digit number for you.\nLet's play a bulls and cows game.")
-    print(oddelovac)
-    return generovane_cislo, oddelovac
+    print(ODDELOVAC)
+    return generovane_cislo, ODDELOVAC
+ 
     
-#spojení funkcí dohromady   
-while True:
-    generovane_cislo, oddelovac = uvod_hry()
-    smycka = True
-    while smycka:
-        hracovo_cislo = input("Enter a number: ")
-        print(oddelovac)
-        if je_tip_validni(hracovo_cislo):
-            #počítáno od chvíle, kdy hráč vložil číslo ve správném fotmátu
-            start_time = time.perf_counter() 
-            pocet_pokusu = 1
-            bull_and_cows = logika_kontrolovani(generovane_cislo,hracovo_cislo)
-            print(f"{bull_and_cows[0]} {pluralize(bull_and_cows[0], 'bull')}, {bull_and_cows[1]} {pluralize(bull_and_cows[1], 'cow')}")
+#spojení funkcí dohromady
+if __name__ == '__main__':   
+    while True:
+        generovane_cislo, oddelovac = uvod_hry()
+        smycka = True
+        while smycka:
+            hracovo_cislo = input("Enter a number: ")
             print(oddelovac)
-            while bull_and_cows[0] != 4:
-                hracovo_cislo = input("Enter a number: ")
+            if je_tip_validni(hracovo_cislo):
+                #počítáno od chvíle, kdy hráč vložil číslo ve správném fotmátu
+                start_time = time.perf_counter() 
+                pocet_pokusu = 1
+                bull_and_cows = logika_kontrolovani(generovane_cislo,hracovo_cislo)
+                print(f"{bull_and_cows[0]} {pluralize(bull_and_cows[0], 'bull')}, {bull_and_cows[1]} {pluralize(bull_and_cows[1], 'cow')}")
                 print(oddelovac)
-                if je_tip_validni(hracovo_cislo):
-                    bull_and_cows = logika_kontrolovani(generovane_cislo, hracovo_cislo)
-                    print(f"{bull_and_cows[0]} {pluralize(bull_and_cows[0], 'bull')}, {bull_and_cows[1]} {pluralize(bull_and_cows[1], 'cow')}")
+                while bull_and_cows[0] != 4:
+                    hracovo_cislo = input("Enter a number: ")
                     print(oddelovac)
-                    pocet_pokusu += 1
-                else:
-                    print("Wrong entered number.")
-                    print(oddelovac)
-            smycka = False
-            end_time = time.perf_counter()
-            celkovy_cas = end_time - start_time
-            print(f"Correct, you've guessed the right number in {pocet_pokusu} guesses and in time of {celkovy_cas:.1f} seconds!")
-        else:
-            print("Wrong entered number.")
-            print(oddelovac)
-    #možnost hráče restartovat hru      
-    restart = input("Would you like to try again? Yes nebo No? ")
-    restart = restart.lower()
-    if restart == "no":
-        print("The game is over.")
-        break
-    elif restart == "yes":
-        continue
+                    if je_tip_validni(hracovo_cislo):
+                        bull_and_cows = logika_kontrolovani(generovane_cislo, hracovo_cislo)
+                        print(f"{bull_and_cows[0]} {pluralize(bull_and_cows[0], 'bull')}, {bull_and_cows[1]} {pluralize(bull_and_cows[1], 'cow')}")
+                        print(oddelovac)
+                        pocet_pokusu += 1
+                    else:
+                        print("Wrong entered number.")
+                        print(oddelovac)
+                smycka = False
+                end_time = time.perf_counter()
+                celkovy_cas = end_time - start_time
+                print(f"Correct, you've guessed the right number in {pocet_pokusu} guesses and in time of {celkovy_cas:.1f} seconds!")
+            else:
+                print("Wrong entered number.")
+                print(oddelovac)
+        #možnost hráče restartovat hru      
+        restart = input("Would you like to try again? Yes nebo No? ")
+        restart = restart.lower()
+        if restart == "no":
+            print("The game is over.")
+            break
+        elif restart == "yes":
+            continue
